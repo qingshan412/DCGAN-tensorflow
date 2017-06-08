ff49c2429872af6a0fff0a650ac42447aada375a
qingshan412 <qingshan412@hotmail.com>
2017-06-08 17:12:53 -0400

    modification

--- FeatureFilter.py
+++ FeatureFilter.py
@@ -7,8 +7,11 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s","--RawDir", type=str,
-                    help="directory for raw samples",
+parser.add_argument("-r","--RawDir", type=str,
+                    help="directory for train raw samples",
+                    default = '../data/1_s_4_p')
+parser.add_argument("-s","--SourceDir", type=str,
+                    help="directory for test raw samples",
                     default = '../data/1_s_4_p')
 parser.add_argument("-r","--ResultDir", type=str,
                     help="directory for samples",
@@ -57,6 +60,47 @@ for Pic in PicFiles:
         AllPx = np.vstack((AllPx, line6))
 
     countflag = countflag + 1
+
+PicPath = args.SourceDir
+PicFiles = [ f for f in listdir(PicPath) if path.isfile(path.join(PicPath,f)) and f.strip().split('.')[-1]=='png']
+
+print('2nd!\nThere are ' + str(len(PicFiles)) + ' files')
+
+#px_per_pic = 1082
+#Nsensor = 9impo
+threshold = args.Threshold
+
+### Read Pixels Out
+countflag = 0
+for Pic in PicFiles:
+    tmp = Image.open(path.join(PicPath, Pic))
+    print('\n\n\n' + str(countflag) + ':')
+    line3 = np.array(tmp.convert('L'))
+    line3 = line3.flatten()
+    #line3 = np.vsplit(line3, 8)
+    #line4 = [np.hsplit(item, 8) for item in line3]
+
+    #for i in xrange(len(line4)):
+    #    item = line4[i]
+    #    for j in xrange(len(item)):
+    #        if i == 0 and j == 0:
+    #            line5 = np.reshape(item[j],(1082, ))
+    #        else:
+    #            line5 = np.vstack((line5, np.reshape(item[j],(1082, ))))
+            
+    line6 = line3 > threshold*255
+    line6 = 1*line6
+    print(np.amax(line6))
+    print(np.amin(line6))
+
+    #if countflag == 0:
+    #    AllPx = line6
+    #else:
+    #    AllPx = np.vstack((AllPx, line6))
+    AllPx = np.vstack((AllPx, line6))
+    
+    countflag = countflag + 1
+
 
 print('end_data')
 
